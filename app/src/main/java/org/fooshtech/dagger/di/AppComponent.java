1902613121751da2c3a62d9d5cb9029d5d186d3c
muhannd79 <muhannddh@gmail.com>
2020-03-01 19:04:11 -0600

    add BaseApplication(Clint) and the AppComponent(Service)

---
+++ app/src/main/java/org/fooshtech/dagger/di/AppComponent.java
@@ -0,0 +1,27 @@
+package org.fooshtech.dagger.di;
+
+import android.app.Application;
+
+import org.fooshtech.dagger.BaseApplication;
+
+import dagger.BindsInstance;
+import dagger.Component;
+import dagger.android.AndroidInjector;
+import dagger.android.support.AndroidSupportInjectionModule;
+
+@Component(
+        modules = {
+                AndroidSupportInjectionModule.class,
+        }
+)
+public interface AppComponent extends AndroidInjector<BaseApplication> {
+
+    @Component.Builder
+    interface Builder{
+
+        @BindsInstance
+        Builder application(Application application);
+
+        AppComponent build();
+    }
+}
